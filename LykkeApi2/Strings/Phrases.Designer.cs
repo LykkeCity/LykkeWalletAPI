a5c9925d0f8468b164e6aaf673045c93fd772ce1
Dafinka Gerdjikova <dafinka.gerdjikova@programista.pro>
2017-10-03 10:08:55 +0300

    Add some changes to Phrases.Designer

--- LykkeApi2/Strings/Phrases.Designer.cs
+++ LykkeApi2/Strings/Phrases.Designer.cs
@@ -8,10 +8,11 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace LykkeApi2.Strings {
+namespace LykkeApi2.Strings
+{
     using System;
-    
-    
+
+
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -22,193 +23,268 @@ namespace LykkeApi2.Strings {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Phrases {
-        
+    internal class Phrases
+    {
+
         private static global::System.Resources.ResourceManager resourceMan;
-        
+
         private static global::System.Globalization.CultureInfo resourceCulture;
-        
+
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Phrases() {
+        internal Phrases()
+        {
         }
-        
+
         /// <summary>
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager {
-            get {
-                if (object.ReferenceEquals(resourceMan, null)) {
+        internal static global::System.Resources.ResourceManager ResourceManager
+        {
+            get
+            {
+                if (object.ReferenceEquals(resourceMan, null))
+                {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("LykkeApi2.Strings.Phrases", typeof(Phrases).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
             }
         }
-        
+
         /// <summary>
         ///   Overrides the current thread's CurrentUICulture property for all
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture {
-            get {
+        internal static global::System.Globalization.CultureInfo Culture
+        {
+            get
+            {
                 return resourceCulture;
             }
-            set {
+            set
+            {
                 resourceCulture = value;
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to Record already existsRecord already exists.
         /// </summary>
-        internal static string AlreadyExists {
-            get {
+        internal static string AlreadyExists
+        {
+            get
+            {
                 return ResourceManager.GetString("AlreadyExists", resourceCulture);
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to Sorry, your app version is not supported anymore. Please update your app from Google Play and continue enjoying the service..
         /// </summary>
-        internal static string AndroidUpdateNeededMsg {
-            get {
+        internal static string AndroidUpdateNeededMsg
+        {
+            get
+            {
                 return ResourceManager.GetString("AndroidUpdateNeededMsg", resourceCulture);
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to Asset Not Found.
         /// </summary>
-        public static string AssetNotFound {
-            get {
+        public static string AssetNotFound
+        {
+            get
+            {
                 return ResourceManager.GetString("AssetNotFound", resourceCulture);
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to Asset Pair Not Found.
         /// </summary>
-        public static string AssetPairNotFound {
-            get {
+        public static string AssetPairNotFound
+        {
+            get
+            {
                 return ResourceManager.GetString("AssetPairNotFound", resourceCulture);
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to Client Balance Not Found.
         /// </summary>
-        internal static string ClientBalanceNotFound {
-            get {
+        internal static string ClientBalanceNotFound
+        {
+            get
+            {
                 return ResourceManager.GetString("ClientBalanceNotFound", resourceCulture);
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to Client with the same email is already registered.
         /// </summary>
-        internal static string ClientWithEmailIsRegistered {
-            get {
+        internal static string ClientWithEmailIsRegistered
+        {
+            get
+            {
                 return ResourceManager.GetString("ClientWithEmailIsRegistered", resourceCulture);
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to App version not supported. Please, update..
         /// </summary>
-        internal static string DefaultUpdateNeededMsg {
-            get {
+        internal static string DefaultUpdateNeededMsg
+        {
+            get
+            {
                 return ResourceManager.GetString("DefaultUpdateNeededMsg", resourceCulture);
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to Email not verified.
         /// </summary>
-        internal static string EmailNotVerified {
-            get {
+        internal static string EmailNotVerified
+        {
+            get
+            {
                 return ResourceManager.GetString("EmailNotVerified", resourceCulture);
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to Field should not be empty.
         /// </summary>
-        internal static string FieldShouldNotBeEmpty {
-            get {
+        internal static string FieldShouldNotBeEmpty
+        {
+            get
+            {
                 return ResourceManager.GetString("FieldShouldNotBeEmpty", resourceCulture);
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to Invalid address.
         /// </summary>
-        internal static string InvalidAddress {
-            get {
+        internal static string InvalidAddress
+        {
+            get
+            {
                 return ResourceManager.GetString("InvalidAddress", resourceCulture);
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to Invalid email format.
         /// </summary>
-        internal static string InvalidEmailFormat {
-            get {
+        internal static string InvalidEmailFormat
+        {
+            get
+            {
                 return ResourceManager.GetString("InvalidEmailFormat", resourceCulture);
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to {PropertyName} is not valid.
         /// </summary>
-        internal static string InvalidPropertyValue {
-            get {
+        internal static string InvalidPropertyValue
+        {
+            get
+            {
                 return ResourceManager.GetString("InvalidPropertyValue", resourceCulture);
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to Invalid value.
         /// </summary>
-        internal static string InvalidValue {
-            get {
+        internal static string InvalidValue
+        {
+            get
+            {
                 return ResourceManager.GetString("InvalidValue", resourceCulture);
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to Maximum length is {0}.
         /// </summary>
-        internal static string MaxLength {
-            get {
+        internal static string MaxLength
+        {
+            get
+            {
                 return ResourceManager.GetString("MaxLength", resourceCulture);
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to Minimum length is {0}.
         /// </summary>
-        internal static string MinLength {
-            get {
+        internal static string MinLength
+        {
+            get
+            {
                 return ResourceManager.GetString("MinLength", resourceCulture);
             }
         }
-        
+
+        /// <summary>
+        ///   Looks up a localized string similar to Limit Order Not Found.
+        /// </summary>
+        public static string NoLimitOrder
+        {
+            get
+            {
+                return ResourceManager.GetString("NoLimitOrder", resourceCulture);
+            }
+        }
+
+        /// <summary>
+        ///   Looks up a localized string similar to Limit History Not Found.
+        /// </summary>
+        public static string NoLimitsHistory
+        {
+            get
+            {
+                return ResourceManager.GetString("NoLimitsHistory", resourceCulture);
+            }
+        }
+
+        /// <summary>
+        ///   Looks up a localized string similar to Limit Trade History Not Found.
+        /// </summary>
+        public static string NoLimitTradesHistory
+        {
+            get
+            {
+                return ResourceManager.GetString("NoLimitTradesHistory", resourceCulture);
+            }
+        }
+
         /// <summary>
         ///   Looks up a localized string similar to {0} should not be empty.
         /// </summary>
-        internal static string NotEmptyField {
-            get {
+        internal static string NotEmptyField
+        {
+            get
+            {
                 return ResourceManager.GetString("NotEmptyField", resourceCulture);
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to We are experiencing technical problems. Please try again..
         /// </summary>
-        internal static string TechnicalProblems {
-            get {
+        internal static string TechnicalProblems
+        {
+            get
+            {
                 return ResourceManager.GetString("TechnicalProblems", resourceCulture);
             }
         }
