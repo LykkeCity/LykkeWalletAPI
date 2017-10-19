513c8b010db4ef84b0e2880d3bdac18b26f850c4
Miladin Joksic <miladin.joksic@primeholding.com>
2017-10-19 11:57:36 +0200

    - Added everything related to pledges: micro service client, models, mapper and controller

---
+++ LykkeApi2/Models/Pledges/GetPledgeResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LykkeApi2.Models.Pledges
+{
+    public class GetPledgeResponse
+    {
+        public string Id { get; set; }
+
+        public string ClientId { get; set; }
+
+        public int CO2Footprint { get; set; }
+
+        public int ClimatePositiveValue { get; set; }
+    }
+}
---
+++ LykkeApi2/Models/Pledges/UpdatePledgeResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LykkeApi2.Models.Pledges
+{
+    public class UpdatePledgeResponse
+    {
+        public string Id { get; set; }
+        public int CO2Footprint { get; set; }
+        public int ClimatePositiveValue { get; set; }
+    }
+}
