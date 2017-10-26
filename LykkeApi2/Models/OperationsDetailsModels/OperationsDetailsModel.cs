541f93e56e422a84ea83343cf26be94134f27492
Dafinka Gerdjikova <dafinka.gerdjikova@programista.pro>
2017-10-26 15:05:22 +0300

    Add Controller  for operations details information

---
+++ LykkeApi2/Models/OperationsDetailsModels/OperationsDetailsModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LykkeApi2.Models.OperationsDetailsModels
+{
+    public class OperationsDetailsModel
+    {
+        public string TransactionId { get; set; }
+        public string Comment { get; set; }
+    }
+}
