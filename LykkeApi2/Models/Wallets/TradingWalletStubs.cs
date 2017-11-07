42774adfb1be1b76bc3292a744b0244620902b98
Sergey Polonsky <polonsky.sergey@hotmail.com>
2017-11-07 21:57:17 +0300

    Added stub for trading wallet id and type.

---
+++ LykkeApi2/Models/Wallets/TradingWalletStubs.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace LykkeApi2.Models.Wallets
+{
+    public static class TradingWalletStubs
+    {
+        public static readonly Guid TradingWalletId = Guid.Empty;
+        public const string TradingWalletType = "Trading";
+    }
+}
