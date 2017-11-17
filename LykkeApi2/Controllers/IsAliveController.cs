feab22dff9121755eef42a680beceb2e9a9e5cfc
atarutin <tarutin_andrew@mail.ru>
2017-11-17 18:03:36 +0300

    added IsAlive controller

---
+++ LykkeApi2/Controllers/IsAliveController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.SwaggerGen.Annotations;
+using LykkeApi2.Models;
+using LykkeApi2.Models.IsAlive;
+using System.Net;
+using System;
+
+namespace LykkeApi2.Controllers
+{
+    [Route("api/[controller]")]
+    public class IsAliveController : Controller
+    {
+        [HttpGet]
+        [SwaggerOperation("IsAlive")]
+        [ProducesResponseType(typeof(IsAliveResponse), (int) HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(ErrorResponse), (int) HttpStatusCode.InternalServerError)]
+        public IActionResult Get()
+        {
+            return Ok(new IsAliveResponse
+            {
+                Name = Microsoft.Extensions.PlatformAbstractions.PlatformServices.Default.Application.ApplicationName,
+                Version = Microsoft.Extensions.PlatformAbstractions.PlatformServices.Default.Application
+                    .ApplicationVersion,
+                Env = Environment.GetEnvironmentVariable("ENV_INFO"),
+#if DEBUG
+                IsDebug = true,
+#else
+                IsDebug = false,
+#endif
+            });
+        }
+    }
+}
