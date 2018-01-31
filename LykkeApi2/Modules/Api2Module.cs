649ff9e191ecd52add74e8fd0c467e4ea5217c23
Nikoloz Gamkrelidze <>
2018-01-31 12:53:30 +0400

    best price deviation check removed

--- LykkeApi2/Modules/Api2Module.cs
+++ LykkeApi2/Modules/Api2Module.cs
@@ -49,8 +49,6 @@ namespace LykkeApi2.Modules
 
             builder.RegisterInstance(new DeploymentSettings());
 
-            builder.RegisterInstance(_settings.CurrentValue.OrdersSettings);
-
             builder.RegisterInstance(_settings.CurrentValue.DeploymentSettings);
 
             builder.RegisterInstance<IAssetsService>(
