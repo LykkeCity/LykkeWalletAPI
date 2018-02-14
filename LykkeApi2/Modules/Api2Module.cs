357588955aa6499855bbeeee678665f71b600abf
Nikoloz Gamkrelidze <>
2018-02-14 17:45:19 +0400

    repo removed and substituted with service call

--- LykkeApi2/Modules/Api2Module.cs
+++ LykkeApi2/Modules/Api2Module.cs
@@ -2,11 +2,9 @@
 using System.Linq;
 using Autofac;
 using Autofac.Extensions.DependencyInjection;
-using AzureRepositories.ExchangeSettings;
 using AzureStorage.Tables;
 using Common;
 using Common.Log;
-using Core.ExchangeSettings;
 using Core.Identity;
 using Core.Services;
 using Core.Settings;
@@ -66,11 +64,7 @@ namespace LykkeApi2.Modules
 
             builder.RegisterType<DomainModelConverter>().AsSelf();
             builder.RegisterType<SrvAssetsHelper>().AsSelf().SingleInstance();
-
-            builder.RegisterInstance<IExchangeSettingsRepository>(
-                new ExchangeSettingsRepository(
-                    AzureTableStorage<ExchangeSettingsEntity>.Create(_settings.ConnectionString(x => x.Db.ClientPersonalInfoConnString),
-                        "ExchangeSettings", _log)));
+            
             RegisterDictionaryEntities(builder);            
             BindServices(builder, _settings, _log);
             builder.Populate(_services);
