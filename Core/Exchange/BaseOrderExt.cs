f71ff77c3583a81d3800e61eaf236191f687caf4
Sergey Polonsky <polonsky.sergey@hotmail.com>
2018-02-15 19:00:24 +0300

    Deleted unnecessary code.

--- Core/Exchange/BaseOrderExt.cs
+++ Core/Exchange/BaseOrderExt.cs
@@ -6,18 +6,13 @@ namespace Core.Exchange
     {
         public const string Buy = "buy";
         public const string Sell = "sell";
-
-        public static OrderAction OrderAction(this IOrderBase orderBase)
-        {
-            return orderBase.Volume > 0 ? Enumerators.OrderAction.Buy : Enumerators.OrderAction.Sell;
-        }
-
+        
         public static OrderAction? GetOrderAction(string actionWord)
         {
             if (actionWord.ToLower() == Buy)
-                return Enumerators.OrderAction.Buy;
+                return OrderAction.Buy;
             if (actionWord.ToLower() == Sell)
-                return Enumerators.OrderAction.Sell;
+                return OrderAction.Sell;
 
             return null;
         }
