01dae7f2302fd0f350acc935187e9e89e4e888c4
Dmitry Samodov <samodov.dmitry@gmail.com>
2018-02-18 19:21:50 +0300

    fix in settings

--- LykkeApi2/Settings/APIv2Settings.cs
+++ LykkeApi2/Settings/APIv2Settings.cs
@@ -15,7 +15,7 @@ namespace LykkeApi2.Settings
         public OperationsHistoryServiceClientSettings OperationsHistoryServiceClient { get; set; }
         public MatchingEngineSettings MatchingEngineClient { set; get; }        
         public SessionsSettings SessionsSettings { get; set; }
-`        public FeeCalculatorSettings FeeCalculatorServiceClient { set; get; }
+        public FeeCalculatorSettings FeeCalculatorServiceClient { set; get; }
         public FeeSettings FeeSettings { set; get; }
     }
 
