9829661f0160f9bacf548c2f9653fc8c0d4e54af
Vitalii Dasaev <vitalii.dasaev@lykke.com>
2018-02-20 21:19:33 +0300

    LWDEV-5715 stats method updated

--- LykkeApi2/Controllers/AffiliateController.cs
+++ LykkeApi2/Controllers/AffiliateController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
+using Common.Log;
 using Core.Constants;
 using Lykke.Service.Affiliate.Client;
 using Lykke.Service.Affiliate.Contracts;
@@ -13,6 +14,7 @@ using LykkeApi2.Models.Affiliate;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Rest;
 
 namespace LykkeApi2.Controllers
 {
@@ -24,37 +26,62 @@ namespace LykkeApi2.Controllers
         private readonly IAffiliateClient _affiliateClient;
         private readonly IRequestContext _requestContext;
         private readonly IRateCalculatorClient _rateCalculatorClient;
+        private readonly ILog _log;
 
-        public AffiliateController(IAffiliateClient affiliateClient, IRequestContext requestContext, IRateCalculatorClient rateCalculatorClient)
+        public AffiliateController(IAffiliateClient affiliateClient, IRequestContext requestContext, IRateCalculatorClient rateCalculatorClient, ILog log)
         {
             _affiliateClient = affiliateClient;
             _requestContext = requestContext;
             _rateCalculatorClient = rateCalculatorClient;
-        }
-
-        [HttpGet]
-        [Route("getlinks")]
-        public async Task<IActionResult> Get()
-        {
-            var val = await _affiliateClient.GetLinks(_requestContext.ClientId);
-            return Ok();
+            _log = log;
         }
 
         [HttpGet]
         [Route("stats")]
         [ProducesResponseType(typeof(AffiliateStatisticsResponse), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
         public async Task<IActionResult> Stats()
         {
+            var links = await _affiliateClient.GetLinks(_requestContext.ClientId);
+
+            if (links.Count() == 0)
+            {
+                LinkModel linkModel;
+                try
+                {
+                    const string redirectUrl = "https://lykke.com";
+                    linkModel = await _affiliateClient.RegisterLink(_requestContext.ClientId, redirectUrl);
+                }
+                catch (HttpOperationException e)
+                {
+                    await _log.WriteErrorAsync(nameof(AffiliateController), nameof(Stats), e);
+                    return BadRequest(new { message = e.Response.Content });
+                }
+
+                await _log.WriteInfoAsync(nameof(AffiliateController), nameof(Stats), $"Url: {linkModel.Url}. RedirectUrl: {linkModel.RedirectUrl}");
+
+                return Ok(new AffiliateStatisticsResponse
+                {
+                    Url = linkModel.Url,
+                    RedirectUrl = linkModel.RedirectUrl,
+                    ReferralsCount = 0,
+                    TotalBonus = 0,
+                    TotalTradeVolume = 0
+                });
+            }
+
             var referrals = await _affiliateClient.GetReferrals(_requestContext.ClientId);
 
             var stats = (await _affiliateClient.GetStats(_requestContext.ClientId)).ToList();
 
             var bonuses = await GetAmountInBtc(stats.ToDictionary(x => x.AssetId, x => x.BonusVolume));
-
+            
             var tradeVolumes = await GetAmountInBtc(stats.ToDictionary(x => x.AssetId, x => x.TradeVolume));
 
             return Ok(new AffiliateStatisticsResponse
             {
+                Url = links.First().Url,
+                RedirectUrl = links.First().RedirectUrl,
                 ReferralsCount = referrals.Count(),
                 TotalBonus = bonuses,
                 TotalTradeVolume = tradeVolumes
