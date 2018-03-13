4c75bd30c66a05e8d43a343a8182b9ad24be66f4
Unknown <a.sizov@itransition.com>
2018-03-13 18:57:19 +0300

    LWW-22 Start reimplementation

---
+++ Core/Clients/ISrvBackup.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace Core.Clients
+{
+    public interface ISrvBackup
+    {
+        Task<bool> IsBackupRequired(string clientId);
+        Task<bool> IsBackupRequiredWithoutWalletCheck(string clientId);
+    }
+}
