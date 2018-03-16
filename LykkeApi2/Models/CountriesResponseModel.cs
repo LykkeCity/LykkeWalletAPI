9ecc19cbb54bc43d6818a3f9a0f8bfc8e562dcf6
A.Sizov <a.sizov@itransition.com>
2018-03-16 18:37:09 +0300

    LWW-24 Implementing CountryPhoneCodesController, models and services

---
+++ LykkeApi2/Models/CountriesResponseModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Core.Countries;
+
+namespace LykkeApi2.Models
+{
+    public class CountriesResponseModel
+    {
+        public string Current { get; set; }
+        public IEnumerable<CountryItem> CountriesList { get; set; }
+    }
+}
