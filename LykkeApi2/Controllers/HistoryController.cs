1d4df0c9f08b4b7d9f283823f43eb7bfb633ae8c
Nikoloz Gamkrelidze <>
2018-03-21 06:29:12 +0100

    temp method removed

--- LykkeApi2/Controllers/HistoryController.cs
+++ LykkeApi2/Controllers/HistoryController.cs
@@ -104,32 +104,5 @@ namespace LykkeApi2.Controllers
 
             return Ok(response.Records.Where(x => x != null).Select(x => x.ToResponseModel()));
         }
-        
-        /// <summary>
-        /// Returns latest trades for a given asset pair
-        /// </summary>
-        /// <param name="assetPairId">Id of asset pair</param>
-        /// <param name="take">How many maximum items have to be returned</param>
-        /// <param name="skip">How many items skip before returning</param>
-        /// <returns></returns>
-        [AllowAnonymous]
-        [HttpGet("trades/{assetPairId}")]
-        [SwaggerOperation("GetAllTradesByPairId")]
-        [ProducesResponseType(typeof(ErrorResponse), (int) HttpStatusCode.InternalServerError)]
-        [ProducesResponseType(typeof(IEnumerable<HistoryOperation>), (int) HttpStatusCode.OK)]
-        public async Task<IActionResult> GetAllTrades(
-            string assetPairId,
-            [FromQuery] int take,
-            [FromQuery] int skip)
-        {
-            var response = await _operationsHistoryClient.GetTradesAsync(assetPairId, take, skip);
-            
-            if (response.Error != null)
-            {
-                return StatusCode((int) HttpStatusCode.InternalServerError, response.Error);
-            }
-
-            return Ok(response.Records.Where(x => x != null).Select(x => x.ToResponseModel()));
-        }
     }
 }
