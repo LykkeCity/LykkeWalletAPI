b080baa67da36de4f5d0f7b6ac65b9f1ced50154
A.Sizov <a.sizov@itransition.com>
2018-03-29 11:12:25 +0300

    LWW-27 Refactoring

--- LykkeApi2/Controllers/DictionaryController.cs
+++ LykkeApi2/Controllers/DictionaryController.cs
@@ -1,87 +1,31 @@
-﻿using System.Net;
-using System.Threading.Tasks;
-using Lykke.Service.ClientDictionaries.AutorestClient.Models;
-using Lykke.Service.ClientDictionaries.Client;
-using LykkeApi2.Infrastructure;
-using LykkeApi2.Models.Dictionary;
+﻿using System.Threading.Tasks;
+using Core.Countries;
+using LykkeApi2.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LykkeApi2.Controllers
 {
     [Authorize]
-    [Route("api/dictionary")]
+    [Produces("application/json")]
+    [Route("api/[controller]")]
     public class DictionaryController : Controller
     {
-        private readonly IRequestContext _requestContext;
-        private readonly IClientDictionariesClient _clientDictionariesClient;
+        private readonly ICountryPhoneCodeService _countryPhoneCodeService;
 
-        public DictionaryController(
-            IRequestContext requestContext,
-            IClientDictionariesClient clientDictionariesClient)
+        public DictionaryController(ICountryPhoneCodeService countryPhoneCodeService)
         {
-            _requestContext = requestContext;
-            _clientDictionariesClient = clientDictionariesClient;
-        }
-        
-        [HttpGet("{key}")]
-        [ProducesResponseType(typeof(DataModel), (int) HttpStatusCode.OK)]
-        [ProducesResponseType(typeof(void), (int) HttpStatusCode.BadRequest)]
-        [ProducesResponseType(typeof(void), (int) HttpStatusCode.NotFound)]
-        public async Task<IActionResult> GetKey(string key)
-        {
-            if (!IsValidKey(key))
-                return BadRequest();
-            
-            var response = await _clientDictionariesClient.GetAsync(_requestContext.ClientId, key);
-
-            return response.Error == null ? Ok(new DataModel { Data = response.Data}) : Error(response.Error);
-        }
-        
-        [HttpPost("{key}")]
-        [ProducesResponseType(typeof(void), (int) HttpStatusCode.OK)]
-        [ProducesResponseType(typeof(void), (int) HttpStatusCode.BadRequest)]
-        [ProducesResponseType(typeof(void), (int) HttpStatusCode.NotFound)]
-        public async Task<IActionResult> SetKey(string key, [FromBody] DataModel request)
-        {
-            if (!IsValidKey(key) || !IsValidPayload(request?.Data))
-                return BadRequest();
-            
-            var response = await _clientDictionariesClient.SetAsync(_requestContext.ClientId, key, request.Data);
-
-            return response.Error == null ? Ok() : Error(response.Error);
+            _countryPhoneCodeService = countryPhoneCodeService;
         }
 
-        [HttpDelete("{key}")]
-        [ProducesResponseType(typeof(void), (int) HttpStatusCode.OK)]
-        [ProducesResponseType(typeof(void), (int) HttpStatusCode.BadRequest)]
-        [ProducesResponseType(typeof(void), (int) HttpStatusCode.NotFound)]
-        public async Task<IActionResult> DeleteKey(string key)
+        [HttpGet]
+        public ResponseModel<CountriesResponseModel> GetCountryPhoneCodes()
         {
-            if (!IsValidKey(key))
-                return BadRequest();
-            
-            var response = await _clientDictionariesClient.DeleteAsync(_requestContext.ClientId, key);
-
-            return response.Error == null ? Ok() : Error(response.Error);
-        }
-
-        private IActionResult Error(ErrorModel error)
-        {
-            if (error.Type == ErrorType.NotFound)
-                return NotFound();
-            else
-                return BadRequest();
-        }
-
-        private static bool IsValidKey(string key)
-        {
-            return !string.IsNullOrWhiteSpace(key);
-        }
-
-        private static bool IsValidPayload(string payload)
-        {
-            return payload != null;
+            return ResponseModel<CountriesResponseModel>
+                .CreateOk(new CountriesResponseModel
+                {
+                    CountriesList = _countryPhoneCodeService.GetCountries()
+                });
         }
     }
 }
