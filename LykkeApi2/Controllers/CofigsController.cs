bdbda2108173f17ee2e913556d9a01710fa98ba5
A.Sizov <a.sizov@itransition.com>
2018-04-05 11:28:58 +0300

    LWW-7 Add Endpoint for ConfigData

---
+++ LykkeApi2/Controllers/CofigsController.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using Core.Settings;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace LykkeApi2.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    public class ConfigsController : Controller
+    {
+        private readonly CreditVouchersSettings _creditVouchersSettings;
+
+        public ConfigsController(CreditVouchersSettings creditVouchersSettings)
+        {
+            _creditVouchersSettings = creditVouchersSettings;
+        }
+
+        [HttpGet]
+        [SwaggerOperation("Get")]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(_creditVouchersSettings);
+        }
+    }
+}
