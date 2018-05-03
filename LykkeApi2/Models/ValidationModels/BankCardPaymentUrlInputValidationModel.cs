f5f493c0cfc45e0ecd28a29043d3d319f791a7cd
Galay Mihail <m.galay@itransition.com>
2018-05-03 14:09:49 +0300

    LWW-72 AssetId validation was added

--- LykkeApi2/Models/ValidationModels/BankCardPaymentUrlInputValidationModel.cs
+++ LykkeApi2/Models/ValidationModels/BankCardPaymentUrlInputValidationModel.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using Core;
 using FluentValidation;
+using LkeServices.Operations;
 using Lykke.Service.AssetDisclaimers.Client;
 using LykkeApi2.Strings;
 using Microsoft.AspNetCore.Http;
@@ -12,16 +13,18 @@ namespace LykkeApi2.Models.ValidationModels
     {
         private readonly CachedAssetsDictionary _cachedAssetsDictionary;
         private readonly IAssetDisclaimersClient _assetDisclaimersClient;
+        private readonly SrvDisabledOperations _srvDisabledOperations;
         private readonly string _clientId;
 
         public BankCardPaymentUrlInputValidationModel(
             IHttpContextAccessor httpContextAccessor,
             CachedAssetsDictionary cachedAssetsDictionary,
-            IAssetDisclaimersClient assetDisclaimersClient)
+            IAssetDisclaimersClient assetDisclaimersClient,
+            SrvDisabledOperations srvDisabledOperations)
         {
             _cachedAssetsDictionary = cachedAssetsDictionary;
             _assetDisclaimersClient = assetDisclaimersClient;
-
+            _srvDisabledOperations = srvDisabledOperations;
             _clientId = httpContextAccessor.HttpContext.User?.Identity?.Name;
 
             RegisterRules();
@@ -29,18 +32,38 @@ namespace LykkeApi2.Models.ValidationModels
 
         private void RegisterRules()
         {
-            RuleFor(reg => reg.Email).MustAsync(IsNotApprovedDepositDisclaimers).WithMessage(Phrases.PendingDisclaimer);
+            RuleFor(reg => reg.AssetId).MustAsync(IsNotApprovedDepositDisclaimers).WithMessage(Phrases.PendingDisclaimer);
+            RuleFor(reg => reg.AssetId).MustAsync(IsNotApprovedDepositDisclaimers).WithMessage(Phrases.BtcDisabledMsg);
+
+            RuleFor(reg => reg.Amount).Must(x => x <= 0).WithMessage(x => string.Format(Phrases.FieldShouldNotBeEmptyFormat, nameof(x.Amount)));
+
         }
 
         private async Task<bool> IsNotApprovedDepositDisclaimers(string value, CancellationToken cancellationToken)
         {
             var depositAsset = await _cachedAssetsDictionary.GetItemAsync(value);
 
-            if (string.IsNullOrEmpty(depositAsset.LykkeEntityId)) return true;
-            var checkDisclaimerResult =
-                await _assetDisclaimersClient.CheckDepositClientDisclaimerAsync(_clientId, depositAsset.LykkeEntityId);
+            if (!string.IsNullOrEmpty(depositAsset.LykkeEntityId))
+            {
+                var checkDisclaimerResult =
+                    await _assetDisclaimersClient.CheckDepositClientDisclaimerAsync(_clientId, depositAsset.LykkeEntityId);
 
-            return !checkDisclaimerResult.RequiresApproval;
+                return !checkDisclaimerResult.RequiresApproval;
+            }
+
+            return true;
         }
+
+        private async Task<bool> IsOperationForAssetDisabled(string value, CancellationToken cancellationToken)
+        {
+            return !await _srvDisabledOperations.IsOperationForAssetDisabled(value);
+        }
+
+        private async Task<bool> IsOperationForAssetDisabled(string value, CancellationToken cancellationToken)
+        {
+            return !await _srvDisabledOperations.IsOperationForAssetDisabled(value);
+        }
+
+
     }
 }
