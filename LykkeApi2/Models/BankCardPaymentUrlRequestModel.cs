ec3e99fc6d0c42dbe11442289f5effa2f3e0ee3b
Galay Mihail <m.galay@itransition.com>
2018-05-05 01:56:20 +0300

    LWW-30 BankCardPaymentUrl was moved to PaymentSystemService

--- LykkeApi2/Models/BankCardPaymentUrlRequestModel.cs
+++ LykkeApi2/Models/BankCardPaymentUrlRequestModel.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using Common;
-using Lykke.Contracts.Payments;
-using Lykke.Service.PaymentSystem.Client;
 using Lykke.Service.PaymentSystem.Client.AutorestClient.Models;
-using Lykke.Service.PersonalData.Contract.Models;
 using Newtonsoft.Json;
-using CashInPaymentSystem = Lykke.Service.PaymentSystem.Client.AutorestClient.Models.CashInPaymentSystem;
 
 namespace LykkeApi2.Models
 {
@@ -35,78 +30,12 @@ namespace LykkeApi2.Models
             get
             {
                 if (string.IsNullOrWhiteSpace(DepositOption))
-                    return Models.DepositOption.BankCard;
+                    return Lykke.Service.PaymentSystem.Client.AutorestClient.Models.DepositOption.BankCard;
 
                 return Enum.TryParse(DepositOption, out DepositOption tmpOption)
                     ? tmpOption
-                    : Models.DepositOption.BankCard;
+                    : Lykke.Service.PaymentSystem.Client.AutorestClient.Models.DepositOption.BankCard;
             }
-        }
-
-        public static BankCardPaymentUrlRequestModel Create(PaymentTransactionResponse pt, IPersonalData personalData)
-        {
-            if (pt.PaymentSystem != CashInPaymentSystem.CreditVoucher
-                && pt.PaymentSystem != CashInPaymentSystem.Fxpaygate)
-                throw new Exception("Credit voucher payment system is expect for transactionID:" + pt.Id);
-
-            var info = pt.GetInfo<OtherPaymentInfo>();
-
-            return new BankCardPaymentUrlRequestModel
-            {
-                Address = info.Address,
-                Amount = pt.Amount,
-                AssetId = pt.AssetId,
-                City = info.City,
-                Country = info.Country,
-                Phone = personalData.ContactPhone,
-                Email = personalData.Email,
-                FirstName = info.FirstName,
-                LastName = info.LastName,
-                Zip = info.Zip
-            };
-        }
-        
-        public static BankCardPaymentUrlRequestModel Create(IPersonalData personalData)
-        {
-            return new BankCardPaymentUrlRequestModel
-            {
-                Address = personalData.Address,
-                City = personalData.City,
-                Phone = personalData.ContactPhone,
-                Country = personalData.CountryFromPOA ?? personalData.Country,
-                Email = personalData.Email,
-                FirstName = personalData.FirstName,
-                LastName = personalData.LastName,
-                Zip = personalData.Zip
-            };
-        }
-
-        public string GetCountryIso3Code()
-        {
-            if (string.IsNullOrWhiteSpace(Country))
-                return null;
-
-            if (CountryManager.HasIso3(Country))
-                return Country;
-
-            if (CountryManager.HasIso2(Country))
-                return CountryManager.Iso2ToIso3(Country);
-
-            throw new Exception($"Country code {Country} not found in CountryManager");
-        }
-
-        public string GetCountryIso2Code()
-        {
-            if (string.IsNullOrWhiteSpace(Country))
-                return null;
-
-            if (CountryManager.HasIso2(Country))
-                return Country;
-
-            if (CountryManager.HasIso3(Country))
-                return CountryManager.Iso3ToIso2(Country);
-
-            throw new Exception($"Country code {Country} not found in CountryManager");
         }
     }
 }
