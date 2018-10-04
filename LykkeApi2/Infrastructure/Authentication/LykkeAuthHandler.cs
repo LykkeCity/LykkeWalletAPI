a0a947fa988f7868bd1971e308ce515f080e9539
Mikhail Gerasimov <mikhail.gerasimov@lykke.com>
2018-10-04 20:21:56 +0200

    LWDEV-9757 [Api 2] Add access tokens cache

--- LykkeApi2/Infrastructure/Authentication/LykkeAuthHandler.cs
+++ LykkeApi2/Infrastructure/Authentication/LykkeAuthHandler.cs
@@ -5,6 +5,7 @@ using IdentityModel.AspNetCore.OAuth2Introspection;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
@@ -14,11 +15,24 @@ namespace LykkeApi2.Infrastructure.Authentication
     {
         private readonly ILykkePrincipal _lykkePrincipal;
         private const int LykkeTokenLength = 64;
+        private static IDistributedCache _cache;
 
-        public LykkeAuthHandler(IOptionsMonitor<OAuth2IntrospectionOptions> options, ILoggerFactory logger, UrlEncoder encoder, ISystemClock clock, ILykkePrincipal lykkePrincipal, IDistributedCache cache = null)
-            : base(options, encoder, clock, logger, cache)
+
+        public LykkeAuthHandler(IOptionsMonitor<OAuth2IntrospectionOptions> options, ILoggerFactory logger, UrlEncoder encoder, ISystemClock clock, ILykkePrincipal lykkePrincipal)
+            : base(options, encoder, clock, logger, GetInMemoryCache())
         {
             _lykkePrincipal = lykkePrincipal;
+        }
+
+        // We don't want to use redis
+        private static IDistributedCache GetInMemoryCache()
+        {
+            if (_cache == null)
+            {
+                _cache = new MemoryDistributedCache(new OptionsWrapper<MemoryDistributedCacheOptions>(new MemoryDistributedCacheOptions()));
+            }
+
+            return _cache;
         }
 
         protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
