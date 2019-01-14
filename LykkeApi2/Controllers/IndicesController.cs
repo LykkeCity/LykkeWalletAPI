8215c712dd15c59e2f125ea77afc822817f85d06
Alexey Zelenin <alexey@zelenin.org>
2019-01-14 22:39:32 +0300

    Removed redundant references, added comments, changed exception handling

--- LykkeApi2/Controllers/IndicesController.cs
+++ LykkeApi2/Controllers/IndicesController.cs
@@ -20,6 +20,9 @@ namespace LykkeApi2.Controllers
             _indicesFacadeClient = indicesFacadeClient;
         }
 
+        /// <summary>
+        /// Returns all indices
+        /// </summary>
         [HttpGet("")]
         [ProducesResponseType(typeof(IList<Index>), (int)HttpStatusCode.OK)]
         public async Task<IActionResult> GetAllAsync()
@@ -29,6 +32,9 @@ namespace LykkeApi2.Controllers
             return Ok(result);
         }
 
+        /// <summary>
+        /// Returns index details by asset identifier
+        /// </summary>
         [HttpGet("{assetId}")]
         [ProducesResponseType(typeof(Index), (int)HttpStatusCode.OK)]
         [ProducesResponseType((int)HttpStatusCode.NotFound)]
@@ -45,12 +51,15 @@ namespace LykkeApi2.Controllers
             }
             catch (ClientApiException)
             {
-                return NotFound();
+                return StatusCode((int)HttpStatusCode.InternalServerError);
             }
 
             return Ok(result);
         }
 
+        /// <summary>
+        /// Returns history values by asset identifier and time interval
+        /// </summary>
         [HttpGet("{assetId}/history/{timeInterval}")]
         [ProducesResponseType(typeof(IList<HistoryElement>), (int)HttpStatusCode.OK)]
         [ProducesResponseType((int)HttpStatusCode.NotFound)]
@@ -70,7 +79,7 @@ namespace LykkeApi2.Controllers
             }
             catch (ClientApiException)
             {
-                return NotFound();
+                return StatusCode((int)HttpStatusCode.InternalServerError);
             }
 
             return Ok(result);
