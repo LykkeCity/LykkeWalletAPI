42f6a64d26ae5b19fb6c2e92bfe4bd6ce8a0efe6
Michael Kobzev <michael.kobzev@lykke.com>
2020-12-01 13:00:50 +0300

    lkk investment requests added

---
+++ Core/Repositories/ILkkInvestmentRequestRepository.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Core.Repositories
+{
+    public interface ILkkInvestmentRequestRepository
+    {
+        Task Add(string clientId, string requestId, string amount, string purchaseOption);
+    }
+}
