a4a2656738c11b57d86ab23ab35adac590faf890
Nika Gamkrelidze <nikagamkrelidze233@gmail.com>
2021-06-28 18:12:37 +0400

    LM-1183_managing-whitelisted-addresses temp implementations to unblock web wallet development.

---
+++ LykkeApi2/Models/Whitelistings/CreateWhitelistingRequest.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LykkeApi2.Models.Whitelistings
+{
+    public class CreateWhitelistingRequest : WhitelistingBaseModel
+    {
+        public string Code2Fa { get; set; }
+    }
+}
---
+++ LykkeApi2/Models/Whitelistings/WhitelistingModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace LykkeApi2.Models.Whitelistings
+{
+    public class WhitelistingModel : WhitelistingBaseModel
+    {
+        public string Id { set; get; }
+        public DateTime CreatedAt { set; get; }
+        [JsonConverter(typeof(StringEnumConverter))]
+        public WhitelistingStatus Status { set; get; }
+    }
+}
