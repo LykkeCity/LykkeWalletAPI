e7e28c53d604b95ced18d83319f649699bf7f228
Dmitry Samodov <samodov.dmitry@gmail.com>
2021-09-28 08:04:45 +0300

    LM-1473: update to .net 5

---
+++ LykkeApi2/Infrastructure/SecurityRequirementsOperationFilter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace LykkeApi2.Infrastructure
+{
+    public class SecurityRequirementsOperationFilter : IOperationFilter
+    {
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            if (context.MethodInfo.GetCustomAttributes(typeof(Microsoft.AspNetCore.Authorization.AuthorizeAttribute), false).Any() ||
+                (context.MethodInfo.DeclaringType?.GetCustomAttributes(typeof(Microsoft.AspNetCore.Authorization.AuthorizeAttribute), false).Any() ?? false))
+            {
+                var oauthScheme = new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "oauth2" }
+                };
+
+                operation.Security = new List<OpenApiSecurityRequirement>
+                {
+                    new() { [oauthScheme] = new List<string>() }
+                };
+            }
+        }
+    }
+}
